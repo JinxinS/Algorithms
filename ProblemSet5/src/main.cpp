b2ed186817017029449dd768c55ba3049a5179c4
Jinxin Song <jinxin@kth.se>
2014-11-20 23:35:51 +0000

    sucess

--- ProblemSet5/src/main.cpp
+++ ProblemSet5/src/main.cpp
@@ -13,13 +13,21 @@
 #include <string.h>
 #include <unordered_map>
 #include <sstream>
-
+#include <set>
+#include <limits.h>
 class Node{
 	int id;
-	std::unordered_map<Node*,int> neighbor;
+	bool isVisited;
 	public:
+
+	int shortestpath;
+	std::unordered_map<Node*,int> neighbor;
 	Node():
-		id(){}
+		id(),
+		isVisited(false),
+		shortestpath(INT_MAX),
+		neighbor()
+	{}
 
 	void setid(int i){ id = i;}
 	int getid() const { return id;}
@@ -27,14 +35,41 @@ class Node{
 		neighbor.insert(std::make_pair(v,d));
 	}
 
+	void updateDistance(int d){
+		this->shortestpath = d;
+	}	
+
 	void printNode(){
 		for(auto it = neighbor.begin();it !=neighbor.end();++it ){
 			printf("Node %d -> distance %d \n",it->first->getid(),it->second);
 		}
 	}
+	
 };
 
+void dijkstra(Node* n,std::set<Node*>& nSet){
+	if(n->getid() == 1){
+		n->shortestpath = 0;
+	}
 
+	while(!nSet.empty()){
+		Node* mindst = *(nSet.begin());
+		for(auto it = nSet.begin();it!=nSet.end();++it){
+			Node * n = *it;
+			if( (mindst->shortestpath) > (n->shortestpath) ) mindst = n;		
+		}
+		nSet.erase(mindst);
+
+		for(auto it = (mindst->neighbor).begin();it!=(mindst->neighbor).end();++it){
+			Node* adj_node  = it->first;
+			int weight = it->second;
+			int distance =mindst->shortestpath + weight;
+		        if(distance < adj_node->shortestpath)
+				adj_node->shortestpath = distance;	
+		}
+	}
+
+}
 using namespace std;
 
 int main() {
@@ -51,7 +86,7 @@ int main() {
 	Node* v = new Node[num_of_lines];
 
 	rewind(fp);
-
+	std::set<Node*> nSet;
 	int *array = (int*)malloc(sizeof(int)*num_of_lines);
 	int i = 0;
 	while((read = getline(&line,&len,fp)) != -1){
@@ -59,9 +94,10 @@ int main() {
 		istringstream iss(s);
 
 		v[i].setid(i+1);
+		nSet.insert(v+i);
 		string sub;
 		iss >> sub;  			//skip the current node
-		int node,distance;
+		int node,weight;
 		do
 		{
 			iss >> sub;
@@ -70,19 +106,20 @@ int main() {
 			node = atoi(pch);
 			pch = strtok (NULL, ",");		  
 			if(pch != NULL){
-				distance = atoi(pch);
-				v[i].addNeighbor(v+node-1,distance);
+				weight = atoi(pch);
+				v[i].addNeighbor(v+node-1,weight);
 			}
 		} while (iss);
 		i++;
 	}
 
-
-//	for(int i = 0; i< num_of_lines; ++i)
-//	{
-//		printf("v[%d] %d \n",i,v[i].getid());
-//		v[i].printNode();
-//	}
+	
+	dijkstra(&v[0],nSet);
+		for(int i = 0; i< num_of_lines; ++i)
+		{
+			printf("v[%d] %d \n",i+1,v[i].shortestpath);
+		
+		}
 	delete []v;
 	return 0;
 }
